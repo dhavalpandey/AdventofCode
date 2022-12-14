5d4b1255c5aeffe6f2faec90fb6834101638a51c
Dhaval Pandey <dhavalpandey2007@outlook.com>
2022-12-14 22:51:12 +0000

    14

--- run.py
+++ run.py
@@ -17,43 +17,54 @@ def error():
 
 
 def success():
-    print('\x1b[6;30;42m' + 'Files were created' + '\x1b[0m')
+    print('\x1b[6;30;42m' + 'Task executed correctly.' + '\x1b[0m')
     sys.exit()
 
 
 def push():
-    os.system("git add .")
-    os.system(f"git commit -m {today}")
-    os.system("git push origin main")
+    try:
+        temp_day = str(day)
+        commit_message = 'Day ' + temp_day + ' complete'
 
-    success()
+        os.system("git add .")
+        os.system(f"git commit -m {temp_day}")
+        os.system("git push origin main")
+
+        success()
+
+    except:
+        error()
 
 
 def new_day():
-    input_dir = f"C:/Users/Dhava/Documents/AdventOfCode/Inputs/Day {day}"
-    solution_dir = f"C:/Users/Dhava/Documents/AdventOfCode/Solutions/Day {day}"
-    os.mkdir(input_dir)
-    os.mkdir(solution_dir)
+    try:
+        input_dir = f"C:/Users/Dhava/Documents/AdventOfCode/Inputs/Day {day}"
+        solution_dir = f"C:/Users/Dhava/Documents/AdventOfCode/Solutions/Day {day}"
+        os.mkdir(input_dir)
+        os.mkdir(solution_dir)
 
-    file_name = f".\Solutions\Day {day}\Day{day}.py"
-    input_sample_name = f".\Inputs\Day {day}\Day{day}SampleInput.txt"
-    input_name = f".\Inputs\Day {day}\Day{day}Input.txt"
+        file_name = f".\Solutions\Day {day}\Day{day}.py"
+        input_sample_name = f".\Inputs\Day {day}\Day{day}SampleInput.txt"
+        input_name = f".\Inputs\Day {day}\Day{day}Input.txt"
 
-    code = open(file_name, "x")
-    sample = open(input_sample_name, "x")
-    input = open(input_name, "x")
+        code = open(file_name, "x")
+        sample = open(input_sample_name, "x")
+        input = open(input_name, "x")
 
-    code.write(f'with open("{input_sample_name}", "r") as f:')
-    code.write(f'\n')
-    code.write(f'    input_file = f.read().strip().split("")')
+        code.write(f'with open("{input_sample_name}", "r") as f:')
+        code.write(f'\n')
+        code.write(f'    input_file = f.read().strip().split("")')
 
-    code.write(f'\n\n')
-    code.write(f'def main():')
+        code.write(f'\n\n')
+        code.write(f'def main():')
 
-    code.write(f'\n\n')
-    code.write(f'main()')
+        code.write(f'\n\n')
+        code.write(f'main()')
 
-    success()
+        success()
+
+    except:
+        error()
 
 
 if key_word == "new":
